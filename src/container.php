b4a9bb3f0ad368f8866816e0a97646fd1d0c1ed0
dyl25 <dylan-vst@hotmail.com>
2017-01-25 13:43:34 +0100

    Ajout de PDO et d'un accès à la DB dans le container

--- src/container.php
+++ src/container.php
@@ -5,16 +5,26 @@ $container = $app->getContainer();
 
 // Register component on container
 $container['view'] = function ($container) {
-    
+
     $dir = dirname(__DIR__);
-    
+
     $view = new \Slim\Views\Twig($dir . '/src/views', [
         'cache' => false //$dir . '/tmp/cache'
     ]);
-    
-    // Instantiate and add Slim specific extension
+
+// Instantiate and add Slim specific extension
     $basePath = rtrim(str_ireplace('index.php', '', $container['request']->getUri()->getBasePath()), '/');
     $view->addExtension(new Slim\Views\TwigExtension($container['router'], $basePath));
 
     return $view;
+};
+
+$container['pdo'] = function($container) {
+    $pdo = new PDO('mysql:dbname=cavavin;host=localhost', 'root', 'root');
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    return $pdo;
+};
+
+$container['db'] = function($container) {
+    return new Database($this->pdo);
 };

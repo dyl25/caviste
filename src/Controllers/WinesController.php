d0b7959b347b9e6d4050447489d4caaf1fdac8bf
dyl25 <dylan-vst@hotmail.com>
2017-02-05 14:27:12 +0100

    Modification et factorisation de WinesController

--- src/Controllers/WinesController.php
+++ src/Controllers/WinesController.php
@@ -25,7 +25,6 @@ class WinesController extends Controller {
             $model = $this->loadModel('winesModel');
             $result = $model->search();
             $result = json_encode($result);
-            //$this->render($response, 'wines/index.html.twig', ['wineData' => $result]);
             $response->getBody()->write($result);
         } else {
             $model = $this->loadModel('winesModel');
@@ -35,12 +34,23 @@ class WinesController extends Controller {
         }
     }
 
+    /**
+     * Affiche tous les vins de la base de données
+     * @param RequestInterface $request
+     * @param ResponseInterface $response
+     */
     public function home(RequestInterface $request, ResponseInterface $response) {
         $model = $this->loadModel('winesModel');
         $result = $model->get_all();
-        /* var_dump($result);
-          die(); */
         $this->render($response, 'wines/home.html.twig', ['wines' => $result]);
+    }
+
+    /**
+     * Affiche les informations sur un vin
+     * @param int $id L'id du vin
+     */
+    public function view($id) {
+        true;
     }
 
     /**
@@ -52,25 +62,29 @@ class WinesController extends Controller {
     public function search(RequestInterface $request, ResponseInterface $response, $args) {
         $model = $this->loadModel('winesModel');
         $result = $model->search($args['name']);
-        $result = json_encode($result, JSON_PRETTY_PRINT);
+        $result = json_encode($result);
         $response->getBody()->write($result);
-        //$this->render($response, 'wines/search.html.twig', ['wineData' => $result]);
     }
 
     /**
      * Réception de donnée au format json vie $_POST, les récupérée et insérer 
      * dans la DB et echo un boolean pour dire si l'insertion s'est bien passée
-     * @param RequestInterface $request
-     * @param ResponseInterface $response
      */
-    public function add(RequestInterface $request, ResponseInterface $response) {
-
-        //if(isset($_POST('save'))) {
-        //décodage de la chaine json
-        //envoi vers le modele
-        $model = $this->loadModel('winesModel');
-        return $model->add_wines($name, $year, $grapes, $country, $region, $description, $picture);
-        //}
+    public function add() {
+        if (isset($_SERVER['HTTP_REFERER'])) {
+            if ($_SERVER['HTTP_REFERER'] == "moncellier.localhost") {
+                $name = $_POST['name'];
+                $year = $_POST['year'];
+                $grapes = $_POST['grapes'];
+                $country = $_POST['country'];
+                $region = $_POST['region'];
+                $description = $_POST['description'];
+                $picture = $_POST['picture'];
+                //envoi vers le modele
+                $model = $this->loadModel('winesModel');
+                return $model->add_wines($name, $year, $grapes, $country, $region, $description, $picture);
+            }
+        }
     }
 
     /**
@@ -79,14 +93,21 @@ class WinesController extends Controller {
      * @return boolean True si le vin a bien été modifié sinon false
      */
     public function put($id) {
-        //if(isset()) {
-        //décodage de la chaine json
-        //
-        //envoi vers le modele
-        $model = $this->loadModel('winesModel');
+        if (isset($_SERVER['HTTP_REFERER'])) {
+            if ($_SERVER['HTTP_REFERER'] == "moncellier.localhost") {
+                $name = $_POST['name'];
+                $year = $_POST['year'];
+                $grapes = $_POST['grapes'];
+                $country = $_POST['country'];
+                $region = $_POST['region'];
+                $description = $_POST['description'];
+                $picture = $_POST['picture'];
+                //envoi vers le modele
+                $model = $this->loadModel('winesModel');
 
-        return $model->update_wine($id, $name, $year, $grapes, $country, $region, $description, $picture);
-        //}
+                return $model->update_wine($id, $name, $year, $grapes, $country, $region, $description, $picture);
+            }
+        }
     }
 
     /**
@@ -95,9 +116,13 @@ class WinesController extends Controller {
      * @return boolean True si le vin a bien été supprimé sinon false
      */
     public function delete($id) {
-        $model = $this->loadModel('winesModel');
+        if (isset($_SERVER['HTTP_REFERER'])) {
+            if ($_SERVER['HTTP_REFERER'] == "moncellier.localhost") {
+                $model = $this->loadModel('winesModel');
 
-        return $model->delete_wine($id);
+                return $model->delete_wine($id);
+            }
+        }
     }
 
     /**
@@ -106,7 +131,7 @@ class WinesController extends Controller {
     public function test() {
         $model = $this->loadModel('winesModel');
 
-        var_dump($model->delete_wine('14'));
+        var_dump($model->add_wines('test3', 2017, 'testgrape', 'country', 'regtest', 'descr', 'testPicture'));
     }
 
 }

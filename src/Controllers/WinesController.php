70aa4a61a2a5ae5e6d0d826b9c70f9838267a7d0
mzEpfc <zagai.mehdi@gmail.com>
2017-02-05 16:08:28 +0100

    Supression de l'option PrettyPrint dans WinesController.php

--- src/Controllers/WinesController.php
+++ src/Controllers/WinesController.php
@@ -10,7 +10,7 @@ use \Psr\Http\Message\ResponseInterface;
 class WinesController extends Controller {
     /**
      * Affiche au format JSON tous les vins de la DB si il n'y a pas d'argument 
-     * sinon si un id est spÃ©cifiÃ© le vin avec cet id sera renvoyÃ©
+     * sinon si un id est spécifié le vin avec cet id sera renvoyé
      * @param RequestInterface $request
      * @param ResponseInterface $response
      * @param type $args
@@ -43,13 +43,13 @@ class WinesController extends Controller {
     public function search(RequestInterface $request, ResponseInterface $response, $args) {
         $model = $this->loadModel('winesModel');
         $result = $model->search($args['name']);
-        $result = json_encode($result, JSON_PRETTY_PRINT);
+        $result = json_encode($result);
         $response->getBody()->write($result);
         //$this->render($response, 'wines/search.html.twig', ['wineData' => $result]);
     }
     /**
-     * RÃ©ception de donnÃ©e au format json vie $_POST, les rÃ©cupÃ©rÃ©e et insÃ©rer 
-     * dans la DB et echo un boolean pour dire si l'insertion s'est bien passÃ©e
+     * Réception de donnée au format json vie $_POST, les récupérée et insérer 
+     * dans la DB et echo un boolean pour dire si l'insertion s'est bien passée
      * @param RequestInterface $request
      * @param ResponseInterface $response
      */
@@ -57,7 +57,7 @@ class WinesController extends Controller {
         if (isset($_SERVER['HTTP_REFERER'])) {
             if ($_SERVER['HTTP_REFERER'] == "moncellier.localhost") {
                 //if(isset($_POST('save'))) {
-                //dÃ©codage de la chaine json
+                //décodage de la chaine json
                 //envoi vers le modele
                 $model = $this->loadModel('winesModel');
                 return $model->add_wines($name, $year, $grapes, $country, $region, $description, $picture);
@@ -67,14 +67,14 @@ class WinesController extends Controller {
     }
     /**
      * Modifie les information d'un vin.
-     * @param int $id L'id du vin Ã  modifier.
-     * @return boolean True si le vin a bien Ã©tÃ© modifiÃ© sinon false
+     * @param int $id L'id du vin à modifier.
+     * @return boolean True si le vin a bien été modifié sinon false
      */
     public function put($id) {
         if (isset($_SERVER['HTTP_REFERER'])) {
             if ($_SERVER['HTTP_REFERER'] == 'moncellier.localhost') {
                 //if(isset()) {
-                //dÃ©codage de la chaine json
+                //décodage de la chaine json
                 //
         //envoi vers le modele
                 $model = $this->loadModel('winesModel');
@@ -85,8 +85,8 @@ class WinesController extends Controller {
     }
     /**
      * Supprime un vin.
-     * @param int $id L'id du vin Ã  supprimer.
-     * @return boolean True si le vin a bien Ã©tÃ© supprimÃ© sinon false
+     * @param int $id L'id du vin à supprimer.
+     * @return boolean True si le vin a bien été supprimé sinon false
      */
     public function delete($id) {
         if (isset($_SERVER['HTTP_REFERER'])) {
